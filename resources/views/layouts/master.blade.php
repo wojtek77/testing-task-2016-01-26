0a3383440e295696f6db19f74e19cdbb025c968b
Wojciech Brüggemann <wojtek77@o2.pl>
2016-01-26 10:16:25 +0100

    remove AppzCoder

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -24,14 +24,14 @@
 	                <span class="icon-bar"></span>
 	                <span class="icon-bar"></span>
 	            </button>
-	            <a class="navbar-brand" href="#">CRUD App</a>
+	            <a class="navbar-brand" href="{{ url('camp/vehicles') }}">Vehicle list</a>
 	        </div>
 
 			<div class="collapse navbar-collapse" id="navbar-collapse-1">
 				<ul class="nav navbar-nav navbar-right">
 					@if (Auth::guest())
-						<li><a href="{{ url('/auth/login') }}">Login</a></li>
-						<li><a href="{{ url('/auth/register') }}">Register</a></li>
+<!--						<li><a href="{{ url('/auth/login') }}">Login</a></li>
+						<li><a href="{{ url('/auth/register') }}">Register</a></li>-->
 					@else
 						<li><a href="#">{{ Auth::user()->name }}</a></li>
 						<li><a href="{{ url('/auth/logout') }}">Logout</a></li>
@@ -49,7 +49,7 @@
 	<hr/>
 
 	<div class="container">
-	    &copy; {{ date('Y') }}. Created by <a href="http://www.appzcoder.com">AppzCoder</a>
+	    <!--&copy; {{ date('Y') }}. Created by <a href="http://www.appzcoder.com">AppzCoder</a>-->
 	    <br/>
 	</div>
 
